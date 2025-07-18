c1d054551bb5529d1dbeac45bc0ecbc6621e2dd7
Kyle B <kyleb@Mac.attlocal.net>
2025-07-17 20:16:43 -0500

    circular dependency

--- src/users/providers/users.services.ts
+++ src/users/providers/users.services.ts
@@ -1,14 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject, forwardRef } from "@nestjs/common";
 import { GetUsersParamDto } from "../dtos/get-users-param.dto";
+import { AuthService } from "src/auth/providers/auth.service";
 
 @Injectable()
 export class UsersService {
+    constructor(
+        // Injecting Auth Service
+        @Inject(forwardRef(()=> AuthService))
+        private readonly authService: AuthService
+    ){}
 
     public findAll(
         getUsersParamsDto: GetUsersParamDto,
         limit: number,
         page: number
     ) {
+        const isAuth = this.authService.isAuth();
+        console.log(isAuth);
+
+        // Auth Service
         return [
             {
                 fristname: 'John',
